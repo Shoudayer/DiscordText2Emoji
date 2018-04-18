5c43381adea9a63dc96e18bd6f4a209e9cdc2551
Houdayer Simon <houdayersimon@gmail.com>
2018-04-18 20:54:01 +0200

    Added numbers
    0123456789

--- src/emoji.py
+++ src/emoji.py
@@ -1,13 +1,17 @@
 from sys import argv
 
-def genEmojis(chaine):
-  if chaine=='':
-    chaine = input("texte :")
-  for char in chaine:
-    if char.lower() in 'abcdefghijklmnopqrstuvwxyz':
-      print(":regional_indicator_"+char.lower()+": ", end='')
-    else:
-      print (char,end='')
+def genEmojis(message):
+    EmojiNumbers=['zero','one','two','three','four','five','six','seven','eight','nine']
+    if message=='':
+        message = input("texte :")
+    for char in message:
+        if char.lower() in 'abcdefghijklmnopqrstuvwxyz':
+            print(":regional_indicator_"+char.lower()+": ", end='')
+        elif char.lower() in '0123456789':
+            i=int(char.lower())
+            print(":"+str(EmojiNumbers[i])+":",end='')
+        else:
+            print (char,end='')
 
 if __name__ == '__main__':
     passedArg1=''
